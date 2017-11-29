aa7356daaed1ee5a5298b1a8e7077282206d0800
Sam Rose <samrose3@gmail.com>
2017-11-29 07:39:29 -0500

    Add Vector Icons to Android

--- android/app/src/main/java/com/speakez/MainApplication.java
+++ android/app/src/main/java/com/speakez/MainApplication.java
@@ -3,6 +3,7 @@ package com.speakez;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.wenkesj.voice.VoicePackage;
 import com.horcrux.svg.SvgPackage;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new VoicePackage(),
           new SvgPackage(),
           new RNSoundPackage()
